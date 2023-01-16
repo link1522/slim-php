389083dff07b5cc94624dcd42527ff4a435232b2
Link <link.lin@udngroup.com>
2023-01-16 16:29:32 +0800

    build command by symfony console

---
+++ app/Commands/MyCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Commands;
+
+use App\Services\InvoiceService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MyCommand extends Command {
+  protected static $defaultName = 'app:my-command';
+  protected static $defaultDescription = 'My command';
+
+  public function __construct(private readonly InvoiceService $invoiceService) {
+    parent::__construct();
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output): int {
+    $output->write('Paid Invoices: ' . count($this->invoiceService->getPaidInvoices()), true);
+
+    return Command::SUCCESS;
+  }
+}
